6313fc1389db48b456e92f41b511edbb63fd58e4
Anselm Baird-Smith <abairdsmith@gmail.com>
2024-05-19 19:48:44 +0200

    Removed unused comment.

--- app/src/main/java/com/anselm/location/extensions.kt
+++ app/src/main/java/com/anselm/location/extensions.kt
@@ -7,7 +7,7 @@ val Any.TAG: String
             if (name.length <= 23) name else name.substring(0, 23)// first 23 chars
         } else {
             val name = javaClass.name
-            if (name.length <= 23) name else name.substring(name.length - 23, name.length)// last 23 chars
+            if (name.length <= 23) name else name.substring(name.length - 23, name.length)
         }
     }
 
