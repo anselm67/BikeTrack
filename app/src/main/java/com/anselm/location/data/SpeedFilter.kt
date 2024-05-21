f670e7e6f28689a2ce57615d7802fda3077d845a
Anselm Baird-Smith <abairdsmith@gmail.com>
2024-05-21 15:50:50 +0200

    Fix average and max speed.

--- app/src/main/java/com/anselm/location/data/SpeedFilter.kt
+++ app/src/main/java/com/anselm/location/data/SpeedFilter.kt
@@ -5,13 +5,16 @@ class SpeedFilter : DataFilter {
     private var sumSpeed = 0.0
     override fun update(sample: Sample) {
         if ( lastSample != null ) {
-            val distance = sample.location.distanceTo(lastSample!!.location).toDouble()
+            val lastSample = lastSample!!
+            val distance = sample.location.distanceTo(lastSample.location).toDouble()
             sample.distance = distance
-            sample.totalDistance += distance
+            sample.totalDistance = lastSample.distance + distance
             sumSpeed += sample.location.speed
             sample.avgSpeed = sumSpeed / sample.totalDistance
             if ( sample.location.speed > sample.maxSpeed ) {
                 sample.maxSpeed = sample.location.speed.toDouble()
+            } else {
+                sample.maxSpeed = lastSample.maxSpeed
             }
         }
         lastSample = sample
