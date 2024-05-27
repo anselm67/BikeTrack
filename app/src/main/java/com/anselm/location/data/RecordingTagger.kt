9bb6a00ef55cc20e040494228cef721fb72b4097
Anselm Baird-Smith <abairdsmith@gmail.com>
2024-05-27 19:38:00 +0200

    Use bounds to avoid making to many tagging requetss,

--- app/src/main/java/com/anselm/location/data/RecordingTagger.kt
+++ app/src/main/java/com/anselm/location/data/RecordingTagger.kt
@@ -3,8 +3,11 @@ package com.anselm.location.data
 import android.util.Log
 import com.anselm.location.BuildConfig
 import com.anselm.location.LocationApplication.Companion.app
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.LatLngBounds
 import kotlinx.coroutines.delay
 import kotlinx.serialization.json.Json
+import kotlinx.serialization.json.JsonObject
 import kotlinx.serialization.json.jsonArray
 import kotlinx.serialization.json.jsonObject
 import kotlinx.serialization.json.jsonPrimitive
@@ -15,6 +18,34 @@ import okhttp3.Response
 import java.io.IOException
 
 private const val TAG = "com.anselm.location.Tagger"
+
+class BoundsChecker {
+    var bounds: LatLngBounds? = null
+    fun isWithinBounds(latlng: LatLng): Boolean {
+        if ( bounds == null ) {
+            return false
+        } else {
+            return bounds!!.contains(latlng)
+        }
+    }
+
+    private fun asLatLng(obj: JsonObject?): LatLng? {
+        val lat = obj?.get("lat")?.jsonPrimitive?.content?.toDouble()
+        val lng = obj?.get("lng")?.jsonPrimitive?.content?.toDouble()
+        return if ( lat != null && lng != null )
+            LatLng(lat, lng)
+        else
+            null
+    }
+
+    fun updateBounds(geometry: JsonObject?) {
+        val northEast = asLatLng(geometry?.get("northeast")?.jsonObject) ?: return
+        val southWest = asLatLng(geometry?.get("southwest")?.jsonObject) ?: return
+        bounds = LatLngBounds(southWest, northEast)
+    }
+}
+
+private val boundsChecker = BoundsChecker()
 
 fun tagPosition(latlngString: String) {
     val url = "https://maps.googleapis.com/maps/api/geocode/json?" +
@@ -39,10 +70,10 @@ fun tagPosition(latlngString: String) {
                 val json = response.body?.string()?.let {
                     Json.parseToJsonElement(it)
                 }
-                Log.d(TAG, "Json: $json")
-                val locality = json?.jsonObject?.get("results")
+                val result = json?.jsonObject?.get("results")
                     ?.jsonArray
                     ?.get(0)
+                val locality = result
                     ?.jsonObject
                     ?.get("address_components")
                     ?.jsonArray
@@ -51,8 +82,14 @@ fun tagPosition(latlngString: String) {
                     ?.get("long_name")
                     ?.jsonPrimitive
                     ?.content
+                val bounds = result
+                    ?.jsonObject
+                    ?.get("geometry")
+                    ?.jsonObject
+                    ?.get("bounds")
+                boundsChecker.updateBounds(bounds?.jsonObject)
 
-                Log.d(TAG, "Json: \nLocality: $locality")
+                Log.d(TAG, "Locality: $locality, bounds $bounds")
             }
         }
 
@@ -61,13 +98,11 @@ fun tagPosition(latlngString: String) {
 
 fun tagRecording(recording: Recording) {
     app.launch {
-        val positions = recording.extractLatLng().mapIndexed { index, it ->
-            if (index % 2 == 0) "${it.latitude},${it.longitude}" else null
-        }.filterNotNull()
-
-        positions.forEach {
-            tagPosition(it)
-            delay(1000)
+        recording.extractLatLng().forEach {
+            if ( ! boundsChecker.isWithinBounds(it)) {
+                tagPosition("${it.latitude},${it.longitude}")
+                delay(1000)
+            }
         }
     }
 
