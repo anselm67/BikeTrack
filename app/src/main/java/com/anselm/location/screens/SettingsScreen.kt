d81ddd3c0f093922396a1dc64391a4a4b77aa4b6
Anselm Baird-Smith <abairdsmith@gmail.com>
2024-05-30 13:04:51 +0200

    Progress indicator when rebuilding the catalog.

--- app/src/main/java/com/anselm/location/screens/SettingsScreen.kt
+++ app/src/main/java/com/anselm/location/screens/SettingsScreen.kt
@@ -6,13 +6,19 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import com.anselm.location.LocationApplication.Companion.app
+import com.anselm.location.components.LoadingDisplay
 import com.anselm.location.models.LocalAppViewModel
 
 @Composable
 fun SettingsScreen() {
+    val recordingManager = app.recordingManager
+    val rebuildingCatalog = remember { mutableStateOf(false) }
+
     val appViewModel = LocalAppViewModel.current
     appViewModel
         .updateTitle("Settings")
@@ -23,10 +29,24 @@ fun SettingsScreen() {
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Button (
-            onClick = { app.recordingManager.rebuildCatalog() }
-        ) {
-            Text("Rebuild Catalog")
+        if (rebuildingCatalog.value) {
+            LoadingDisplay()
+        } else {
+            Button(
+                onClick = {
+                    rebuildingCatalog.value = true
+                    app.launch {
+                        try {
+                            recordingManager.rebuildCatalog()
+                        } finally {
+                            rebuildingCatalog.value = false
+                            app.toast("${recordingManager.list().size} rides saved.")
+                        }
+                    }
+                }
+            ) {
+                Text("Rebuild Catalog")
+            }
         }
     }
 }
