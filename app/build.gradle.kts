dd3766d3f1d2680db84b1cfd14a0900e988508c9
Anselm <abairdsmith@gmail.com>
2024-10-23 12:11:50 +0200

    Supports API 35.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,7 +13,7 @@ android {
     defaultConfig {
         applicationId = "com.anselm.location"
         minSdk = 34
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
