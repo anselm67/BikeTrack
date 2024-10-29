7239dee54514fa37f0cc8ae7c2060140e535a6a7
anselm <abairdsmith@gmail.com>
2024-10-29 20:23:19 +0100

    Optimize imports.

--- app/src/main/java/com/anselm/location/screens/ViewRecordingsScreen.kt
+++ app/src/main/java/com/anselm/location/screens/ViewRecordingsScreen.kt
@@ -6,14 +6,11 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
