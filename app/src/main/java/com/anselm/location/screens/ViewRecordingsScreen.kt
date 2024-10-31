f018ddc7795fca258e10dd113140a6621153490d
anselm <abairdsmith@gmail.com>
2024-10-31 18:22:20 +0100

    Code analysis.

--- app/src/main/java/com/anselm/location/screens/ViewRecordingsScreen.kt
+++ app/src/main/java/com/anselm/location/screens/ViewRecordingsScreen.kt
@@ -1,6 +1,5 @@
 package com.anselm.location.screens
 
-import android.util.Log
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
@@ -12,7 +11,6 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
