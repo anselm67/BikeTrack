38da9a38d76a05e261aa3e1b9c5009f7df9bd60a
anselm <abairdsmith@gmail.com>
2024-10-31 19:47:31 +0100

    Code analysis.

--- app/src/main/java/com/anselm/location/screens/ViewRecordingsScreen.kt
+++ app/src/main/java/com/anselm/location/screens/ViewRecordingsScreen.kt
@@ -36,7 +36,6 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.em
 import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.anselm.location.LocalNavController
